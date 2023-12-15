2b90ab1622110638f496fcebaf7aab748cea4e99
Rafael Della Giustina <rafael.della64@gmail.com>
2023-12-15 11:35:01 -0300

    static-final

---
+++ meuTema/js/js.js
@@ -0,0 +1,35 @@
+$('section.clientes-slider .slider-container').slick({
+    dots: true,
+    arrows:false,
+    infinite: false,
+    speed:1000,
+    slidesToShow: 4,
+    autoplay: true,
+    centerMode:false,
+    autoplaySpeed: 3000,
+    pauseOnHover:false,
+    responsive: 
+    [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+    ]
+});
+
+$('section.depoimentos .depoimentos-box').slick({
+    dots: true,
+    arrows:false,
+    infinite: true,
+    speed:1000,
+    slidesToShow: 1,
+    centerMode:false
+});
+</script>
+<script>
+//Menu responsivo.
+$('.menu-mobile i').click(function(){
+    $('.menu-mobile').find('ul').slideToggle();
+})
